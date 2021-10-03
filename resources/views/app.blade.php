6616df61c483076b948b51617bc7e64b0fe41d61
Steve Bauman <6421846+stevebauman@users.noreply.github.com>
2021-10-03 17:10:06 -0400

    Spacing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,33 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LdapRecord Browser</title>
 
-    <title>LdapRecord Browser</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        @livewireStyles
 
-    <!-- Styles -->
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    @livewireStyles
+        <!-- Alpine -->
+        <script src="//unpkg.com/alpinejs" defer></script>
+    </head>
 
-    <!-- Alpine -->
-    <script src="//unpkg.com/alpinejs" defer></script>
-</head>
+    <body class="antialiased min-h-screen bg-gray-100 dark:bg-gray-900 py-4 px-4">
+        <div class="mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-center mb-4">
+                <img src="https://ldaprecord.com/logo.svg" style="width:175px;min-width:175px;height:75px;">
+            </div>
 
-<body class="antialiased min-h-screen bg-gray-100 dark:bg-gray-900 py-4 px-4">
-    <div class="mx-auto sm:px-6 lg:px-8">
-        <div class="flex justify-center mb-4">
-            <img src="https://ldaprecord.com/logo.svg" style="width:175px;min-width:175px;height:75px;">
+            {{ $slot }}
         </div>
 
-        {{ $slot }}
-    </div>
-
-    @livewireScripts
-</body>
-
+        @livewireScripts
+    </body>
 </html>
