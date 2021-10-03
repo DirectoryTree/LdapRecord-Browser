e62aa5dcb08f7aa425e0929f4badacdf23720488
Steve Bauman <6421846+stevebauman@users.noreply.github.com>
2021-10-03 17:07:42 -0400

    Spacing

--- resources/views/livewire/connections.blade.php
+++ resources/views/livewire/connections.blade.php
@@ -1,7 +1,8 @@
 <ul class="flex flex-col space-y-4 md:mx-auto max-w-sm">
     @forelse($connections as $connection)
     <li
-        class="relative flex bg-white shadow overflow-hidden rounded-md px-6 py-4 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+        class="relative flex bg-white shadow overflow-hidden rounded-md px-6 py-4 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+    >
         <a href="{{ route('ldap::browse', $connection) }}" class="focus:outline-none inline-flex items-center">
             <span class="absolute inset-0" aria-hidden="true"></span>
             <x-ldap::icons.connection class="mr-2 h-8 w-8 text-gray-600" />
