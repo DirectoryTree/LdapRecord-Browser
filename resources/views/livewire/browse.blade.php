d5ca7a968880db8d0ce59352cdd7d9f1f90b816e
Steve Bauman <6421846+stevebauman@users.noreply.github.com>
2021-10-03 17:07:59 -0400

    Add "jump to" attribute section

--- resources/views/livewire/browse.blade.php
+++ resources/views/livewire/browse.blade.php
@@ -1,13 +1,27 @@
-<div class="flex flex-col md:flex-row md:space-x-4">
+<div class="flex flex-col lg:flex-row md:space-x-4 max-w-7xl mx-auto">
     <livewire:ldap.flash />
 
-    <div class="w-full h-full md:w-1/4 bg-white shadow rounded-lg mb-4 text-sm">
+    <div class="w-full h-full lg:w-1/4 bg-white shadow rounded-lg mb-4 text-sm">
         <livewire:ldap.search />
 
         <livewire:ldap.tree />
     </div>
 
-    <div class="w-full md:w-3/4">
+    <div class="w-full {{ $guid ? 'lg:w-2/4' : 'lg:w-3/4' }}">
         <livewire:ldap.viewer />
     </div>
+
+    @if($guid)
+    <div class="w-full h-full lg:w-1/4 bg-white shadow rounded-lg relative">
+        <div class="text-center tracking-wide uppercase text-gray-600 text-sm border-b py-2">Jump to</div>
+
+        <div class="p-4 space-y-1">
+            @foreach($this->attributes->keys() as $attribute)
+            <div>
+                <a href="#{{ $attribute }}" class="underline hover:no-underline text-indigo-700 text-sm">{{ $attribute }}</a>
+            </div>
+            @endforeach
+        </div>
+    </div>
+    @endif
 </div>
--- src/Livewire/Browse.php
+++ src/Livewire/Browse.php
@@ -6,6 +6,39 @@ use Livewire\Component;
 
 class Browse extends Component
 {
+    use ViewsModel;
+
+    /**
+     * The query string watcher.
+     *
+     * @var array
+     */
+    protected $queryString = [
+        'guid'=> ['except'=> ''],
+    ];
+    
+    /**
+     * The events to listen for.
+     *
+     * @var array
+     */
+    protected $listeners = [
+        'model.selected' => 'selected',
+    ];
+
+    /**
+    * Load the selected entry.
+    *
+    * @param string $guid
+    *
+    * @return void
+    */
+    public function selected($guid)
+    {
+        $this->guid = $guid;
+    }
+
+
     /**
      * Render the component.
      *
